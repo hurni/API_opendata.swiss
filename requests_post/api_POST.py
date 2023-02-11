084180b3fa30820ab31c0c9059d2688b0d04f8ca
hurni <71129916+hurni@users.noreply.github.com>
2023-02-11 16:24:44 +0100

    Update api_POST.py

--- requests_post/api_POST.py
+++ requests_post/api_POST.py
@@ -28,7 +28,7 @@ now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.0000")
 # id of dataset to be changed: "6fbab091-823e-43df-8b1d-de2c3e6763c9"
 ids = '6fbab091-823e-43df-8b1d-de2c3e6763c9'
 
-private_api_key = '0887bd41-1637-4334-a0ad-633b0f557667'
+private_api_key = YOUR_API_KEY # see https://ckan.opendata.swiss/de/user/YOUR_USER_NAME
 url = 'https://ckan.ogdch-abnahme.clients.liip.ch/api/3/action/package_patch'
 
 
