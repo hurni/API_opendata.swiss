1277c52403057a1fa09ffb9b7458f13f6162905d
hurni <71129916+hurni@users.noreply.github.com>
2023-09-13 10:53:48 +0200

    Update api_POST.py

--- requests_post/api_POST.py
+++ requests_post/api_POST.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 """
+Created on Tue Mar 21 12:37:15 2023
 
-how to POST updates of dataset's metadata (from within the BVerwaltungsnetz).
-
-
-
+@author: U80838962
 """
 
-import requests
 import urllib3
-from datetime import datetime
-
+import requests
+import random
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 proxies = {
           'http':'http://proxy-bvcol.admin.ch:8080',
           'https':'http://proxy-bvcol.admin.ch:8080'
@@ -20,25 +18,96 @@ proxies = {
 
 s = requests.Session()
 
-now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.0000")
-#iso_now = now.isoformat(timespec='milliseconds')
 
-#print(now)
+# from dummy user - not the sys-admin
+private_api_key = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX' # cp. opendata.swiss > user page (click on your name/icon in the top right corner)
 
-# id of dataset to be changed: "6fbab091-823e-43df-8b1d-de2c3e6763c9"
-ids = '6fbab091-823e-43df-8b1d-de2c3e6763c9'
+url = 'https://ckan.ogdch-abnahme.clients.liip.ch/api/3/action/package_create'
 
-private_api_key = YOUR_API_KEY # see https://ckan.opendata.swiss/de/user/YOUR_USER_NAME
-url = 'https://ckan.ogdch-abnahme.clients.liip.ch/api/3/action/package_patch'
+header = {
+    'Authorization': private_api_key
+    }
 
+# random nummer added to enable multiple usage
+randomizer = str(random.randrange(100000))
 
-header = {'Authorization': private_api_key}
-
+# resource part works when using url = '...action/resource_create'
 data = {
-        'id': ids,
-        'modified' : now
+        "name": "name_package2"+randomizer, 
+        "identifier": "namet_package2"+randomizer+"@bundesamt-fuer-open-data",
+        "title_for_slug": "name_package2"+randomizer, 
+        "display_name": {
+        	"fr": "display_name test_API_POST package fr", 
+        	"de": "display_name test_API_POST package de", 
+        	"en": "display_name test_API_POST package en", 
+        	"it": "display_name test_API_POST package it"
+        	}, 
+        "publisher": {
+        	"url": 'https://opendata.swiss/organization/bundesamt-fuer-open-data',
+        	"name": ""
+        	}, 
+        
+        "issued": "2022-03-19T00:00:00", 
+        "contact_points": {}, 
+        
+        "description": {
+        	"fr": "description test_API_POST package fr", 
+        	"en": "description test_API_POST package en", 
+        	"de": "description test_API_POST package de", 
+        	"it": "description test_API_POST package it"
+        	}, 
+        "title": {
+        	"fr": "title test_API_POST package fr", 
+        	"de": "title test_API_POST package de", 
+        	"en": "title test_API_POST package en", 
+        	"it": "title test_API_POST package it"
+        	}, 
+        
+        "private": False, 
+        "isopen" : False,
+        "type": "dataset", 
+        "accrual_periodicity": "http://publications.europa.eu/resource/authority/frequency/MONTHLY", 
+        "owner_org": "98dda3ef-6f39-4e3a-b133-29dd2bec632d", 
+        "url": "example.com", 
+        
+        "resources": [{
+        	"issued": "2023-02-13T00:00.0000", 
+        	"display_name": {
+        		"fr": "display_name test_API_POST resource fr", 
+        		"de": "display_name test_API_POST resource de", 
+        		"en": "display_name test_API_POST resource en", 
+        		"it": "display_name test_API_POST resource it"
+        		}, 
+        	"title": {
+        		"fr": "title test_API_POST resource fr", 
+        		"de": "title test_API_POST resource de", 
+        		"en": "title test_API_POST resource en", 
+        		"it": "title test_API_POST resource it"
+        		}, 
+        	"download_url": "example.com",
+        	"state": "active", 
+        	"description": {
+        		"fr": "description test_API_POST resource fr", 
+        		"en": "description test_API_POST resource en", 
+        		"de": "description test_API_POST resource de", 
+        		"it": "description test_API_POST resource it"
+        		}, 
+        
+        	"name": {
+        		"fr": "name test_API_POST resource fr", 
+        		"de": "name test_API_POST resource de", 
+        		"en": "name test_API_POST resource en", 
+        		"it": "name test_API_POST resource it"
+        		}, 
+        	"rights": "NonCommercialAllowed-CommercialAllowed-ReferenceNotRequired", 
+        	"url": "https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_200KB_CSV-1.csv", 
+        	"identifier": "name_resource_2@bundesamt-fuer-open-data", 
+        	}], 
+        
         }
 
-x = s.post(url,data=data,proxies=proxies,headers=header,verify=False)
-print(x.status_code)
 
+
+print(data)
+x = s.post(url, json=data, headers=header, proxies=proxies,verify=False)
+print(x.status_code)
